fa8f8364b7e5132664aad8472baf114454ea9129
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-28 13:44:59 -0700

    Add index and js file, add +-*/ functions

---
+++ calculator.js
@@ -0,0 +1,36 @@
+//Calculator Project - The Odin Projecy
+//Tom Hergenreter 2023
+
+function operate(num1, num2, operator){
+switch (operator) {
+    case 'add': add(num1, num2);
+    break;
+    case 'subtract': subtract(num1, num2);
+    break;
+    case 'multiply': multiply(num1, num2);
+    break;
+    case 'divide': divide(num1, num2);
+    break;
+}
+}
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+function subtract(num1, num2){
+    console.log(num1 - num2);
+}
+
+function multiply(num1, num2){
+    console.log(num1 * num2);
+}
+
+function divide(num1, num2){
+    console.log(num1 / num2);
+}
+
+operate(2,2,'add')
+operate(2,2,'subtract')
+operate(2,2,'multiply')
+operate(2,2,'divide');
